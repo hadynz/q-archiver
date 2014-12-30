ca407e1fd7a440636e1fb5e6884461ac63399b35
Hady Osman <hadyos@gmail.com>
2014-12-30 16:56:35 +1300

    Complete initial Q-Archiver implementation with unit tests

--- test/q-archiver.js
+++ test/q-archiver.js
@@ -1,36 +1,56 @@
 var QArchiver = require('../lib/q-archiver'),
+  fs = require('fs-extra'),
   chai = require('chai'),
-  assert = chai.assert;
+  assert = chai.assert,
+  tmp = __dirname + '/tmp',
+  zip = require('zipfile');
 
 describe('q-archiver', function () {
+
+  before(function(){
+    fs.mkdirsSync(tmp);
+  });
+
+  after(function(){
+    fs.removeSync(tmp);
+  });
 
   it('should exist', function () {
     assert.ok(QArchiver);
   });
 
-  it('can be initialized', function () {
-    var qArchiver = new QArchiver({});
+  it('can be initialised if a filepath is provided', function () {
+    var qArchiver = new QArchiver('filepath');
     assert.ok(qArchiver);
   });
 
-  /*
-  it('will fail when an invalid target filename is used', function () {
-
-//    var qArchiver = new QArchiver('/ \0 < > : invalid file name.ext');
-    var qArchiver = new QArchiver(__dirname + '/bulk-output.zip', 'zip', { cwd: 'fixtures/', src: ['*.txt'] });
-
-    qArchiver.archive()
-      .then(function(){
-        console.log('&&&&&&& Sucessful Path! &&&&&&&', arguments);
-        expect(true).to.equal(false);
-        immediatelyFail();
-      })
-      .fail(function(error){
-        expect(error).to.exist
-          .and.be.instanceof(Object);
-      })
-      .done();
+  it('will throw an exception if filepath is not provided', function () {
+    assert.throws(QArchiver, Error);
   });
-*/
+
+  describe('bulk', function () {
+    var actual;
+
+    before(function(done){
+      var filename = tmp + '/bulk-output.zip',
+        qArchiver = new QArchiver(filename),
+        mappings = [
+          { expand: true, cwd: 'test/fixtures/', src: '*' }
+        ];
+
+      qArchiver
+        .bulk(mappings)
+        .then(function(){
+          actual = new zip.ZipFile(filename);
+          done();
+        });
+    });
+
+    it('should compress two files', function () {
+      assert.equal(actual.count, 2);
+      assert.equal(actual.names.length, 2);
+    });
+
+  });
 
 });
